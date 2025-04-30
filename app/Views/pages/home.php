3a03a27dcd1c43610956e33c6a371454ef0a13b2
MetronARC <ricoroenaldo@metronarc.com>
2025-04-30 17:19:05 +0700

    video

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -21,7 +21,7 @@
           <p>Real-time insights for enhanced efficiency, reduced rework costs, and unwavering quality assurance for your welding operations</p>
           <div class="d-flex">
             <a href="#contact" class="btn-get-started">Contact us</a>
-            <a href="https://www.youtube.com/watch?v=IxX_QHay02M&ab_channel=W%26W" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Demo Video</span></a>
+            <a href="https://www.youtube.com/watch?v=OCZbUGj-t2M&ab_channel=WarrenBuffalo" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Demo Video</span></a>
           </div>
         </div>
       </div>
